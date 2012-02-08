3e91d926efe3e3263df1a9bba5c88ae32fb9a47d
Jon "The Nice Guy" Spriggs <jon@sprig.gs>
2012-02-08 21:52:27 +0000

    This seems to be a better way of doing hooks. Note, plugin developers can add their own hooks too. Documentation to follow.

--- classes/base/hook.php
+++ classes/base/hook.php
@@ -2,8 +2,76 @@
 
 class base_hook
 {
-    function add($object)
+    protected static $self = null;
+    protected $arrHooks = array();
+    protected $arrTriggers = array(
+        'cronTick' => true,
+        'apiRender' => true,
+        'httpRender' => true,
+        'userLogin' => true,
+        'deviceLogin' => true,
+        'createRecord' => true,
+        'updateRecord' => true,
+        'deleteRecord' => true
+    );
+
+    protected function getSelf()
     {
-        // use the method_exists($object, 'function_name'); to check whether the various hooks can be added.
+        if (self::$self == null) {
+            self::$self = new self();
+        }
+        return self::$self;
+    }
+
+    protected function __construct()
+    {
+        require_once(dirname(__FILE__) . '/../../config/plugin.php');
+    }
+
+    public function addTrigger($strTrigger = null)
+    {
+        if ($strTrigger == null) {
+            return false;
+        }
+        $this->arrTriggers[$strTrigger] = true;
+    }
+    
+    public function addHook($strAction = null, $objHook = null)
+    {
+        if (is_object($objHook)) {
+            $self = self::getSelf();
+            $boolTriggerSet = false;
+            foreach ($self->arrTriggers as $strTrigger => $dummyValue) {
+                if (method_exists($objHook, 'hook_' . $strTrigger)) {
+                    $self->arrHooks[$strTrigger][] = $objHook;
+                    $boolTriggerSet = true;
+                }
+            }
+            if ($boolTriggerSet == false) {
+                throw new Exception('No recognised hooks', 0);
+            }
+        } else {
+            throw new Exception('Not an object', 1);
+        }
+    }
+
+    public function triggerHook($strAction = null, $parameters = null)
+    {
+        echo "Trigger Hooks - $strAction\r\n";
+        if ($strAction == null) {
+            throw new Exception('No hooks triggered', 0);
+        } else {
+            $self = self::getSelf();
+            foreach ($self->arrTriggers as $strTrigger => $dummyValue) {
+                if ($strAction != $strTrigger) {
+                    throw new Exception('Invalid hook triggered', 1);
+                }
+            }
+            $strHookAction = 'hook_' . $strAction;
+            foreach ($self->arrHooks[$strAction] as $objHook) {
+                $objHook->$strHookAction($parameters);
+            }
+        }
     }
 }
+
