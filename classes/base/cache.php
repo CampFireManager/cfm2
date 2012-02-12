747768d95bc22267ca0c371f85a784ff41e11ae6
Jon "The Nice Guy" Spriggs <jon@sprig.gs>
2012-02-12 00:09:35 +0000

    This is a new class which performs all the caching for the brokers in all the classes.

---
+++ classes/base/cache.php
@@ -0,0 +1,22 @@
+<?php
+
+class base_cache
+{
+    // Broker Requirements
+    public $arrCache = array();
+    protected static $handler = null;
+
+    /**
+     * An internal function to make this a singleton. This should only be used when being used to find objects of itself.
+     *
+     * @return object This class by itself.
+     */
+    public static function getHandler()
+    {
+        if (self::$handler == null) {
+            self::$handler = new self();
+        }
+        return self::$handler;
+    }
+
+}
