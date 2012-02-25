b10b7f8af4b52bd719af191a16304d9eb31cf45a
Jon "The Nice Guy" Spriggs <jon@sprig.gs>
2012-02-25 00:41:40 +0000

    Use the media handler to identify whether the data types being requested are appropriate for the types of data being supplied. Supply Timetable data if relevant. As a temporary measure, return data in a vaguely handleable format.

--- index.php
+++ index.php
@@ -5,9 +5,38 @@ $generator = microtime(true);
 require_once dirname(__FILE__) . '/classes/autoloader.php';
 
 $arrRequestData = Base_Request::getRequest();
+$arrMediaType = explode('/', $arrRequestData['strPreferredAcceptType']);
 
 // What type of request is this
 $rest = false;
+$media = false;
+
+if (is_array($arrRequestData['pathItems']) && count($arrRequestData['pathItems']) > 0) {
+    if ($arrRequestData['pathItems'][0] == 'media') {
+        unset($arrRequestData['pathItems'][0]);
+        $media = Base_Request::getMediaType('media');
+        if (! $media) {
+            Base_Response::sendHttpResponse(404, null, $arrRequestData['strPreferredAcceptType']);
+        }
+        $file = Base_Config::getConfigLocal('strMediaPath', dirname(__FILE__) . '/media');
+        foreach ($arrRequestData['pathItems'] as $key => $pathItem) {
+            if ($key != 0) {
+                if ($pathItem == '..') {
+                    Base_Response::sendHttpResponse(403, null, $arrRequestData['strPreferredAcceptType']);
+                }
+                $file .= '/' . $pathItem;
+            }
+        }
+        if (is_file($file)) {
+            Base_Response::dl_file_resumable($file, TRUE, $arrRequestData['strPreferredAcceptType']);
+        }
+    }
+    if ($arrRequestData['pathItems'][0] == 'rest' && Base_Request::getMediaType('rest')) {
+        unset($arrRequestData['pathItems'][0]);
+        $rest = true;
+    }
+}
+    
 // What type of objects can we request
 $object = array('config' => 'Base_Config');
 
@@ -23,11 +52,8 @@ $useObjects = array();
 $arrObjects = array();
 $arrObjectsData = array();
 
-if (is_array($arrRequestData['pathItems']) && count($arrRequestData['pathItems']) > 0) {
+if (is_array($arrRequestData['pathItems']) && count($arrRequestData['pathItems']) > 0 && $arrRequestData['pathItems'][0] != '') {
     foreach ($arrRequestData['pathItems'] as $pathItem) {
-        if ($pathItem == 'rest') {
-            $rest = true;
-        }
         if (isset($object[$pathItem])) {
             $useObjects[$object[$pathItem]] = null;
             $lastObject = $pathItem;
@@ -44,15 +70,17 @@ if (is_array($arrRequestData['pathItems']) && count($arrRequestData['pathItems']
             $arrObjects[$object][$item] = $object::brokerByID($item);
         }
     }
+} else {
+    $timetable = new Collection_Timetable();
+    $arrObjectsData['Collection_Timetable'] = $timetable->getData();
 }
-
 
 $useObjects['Object_User'] = 'current';
 $arrObjects['Object_User']['current'] = Object_User::brokerCurrent();
 
 foreach ($arrObjects as $object_group => $data) {
     foreach ($data as $key => $object) {
-        if ($object !== false && $object !== null) {
+        if (is_object($object)) {
             $object->setFull(true);
             $arrObjectsData[$object_group][$key] = $object->getSelf();
         } else {
@@ -61,13 +89,18 @@ foreach ($arrObjects as $object_group => $data) {
     }
 }
 
-var_dump(
-        array(
-            'RESTful' => $rest, 
-            'Objects' => array(
-                'Items' => $useObjects, 
-                'Objects' => $arrObjectsData
-                ), 
-            'generator' => round(microtime(true) - $generator, 3) . ' seconds'
-            )
-        );
+if ($rest) {
+    switch ($arrRequestData['strPreferredAcceptType']) {
+    case 'application/json':
+        Base_Response::sendHttpResponse(200, Base_Response::utf8json($arrObjectsData), $arrRequestData['strPreferredAcceptType']);
+        break;
+    case 'text/xml':
+        Base_Response::sendHttpResponse(200, Base_Response::utf8xml($arrObjectsData), $arrRequestData['strPreferredAcceptType']);
+        break;
+    case 'text/html':
+        Base_Response::sendHttpResponse(200, Base_Response::utf8html($arrObjectsData), $arrRequestData['strPreferredAcceptType']);
+        break;
+    }
+} else {
+    Base_Response::sendHttpResponse(200, Base_Response::utf8html($arrObjectsData), $arrRequestData['strPreferredAcceptType']);
+}
