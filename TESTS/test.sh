58bc8ffbbf5ef758624662deb9479feee41a91d9
Jon "The Nice Guy" Spriggs <jon@sprig.gs>
2012-02-28 08:33:03 +0000

    Update the Test script to refer to the valid paths

--- TESTS/test.sh
+++ TESTS/test.sh
@@ -8,7 +8,7 @@ if [ -z $2 ]; then
   echo "Checking syntax... "
   rm -Rf /tmp/phpcs_stats "$BASE_DIR/../DOCS/" /tmp/testing_status
   mkdir "$BASE_DIR/../DOCS"
-  find "$BASE_DIR/.." -name "*.php" -not -wholename "*/EXTERNALS/*/*" -not -wholename "*/TEMPLATES/C*" -exec "$0" "$1" '{}' \;
+  find "$BASE_DIR/.." -name "*.php" -not -wholename "*/ExternalLibraries/*/*" -not -wholename "*/Templates/C*" -exec "$0" "$1" '{}' \;
   echo "Done."
   echo "Checking classes... "
   php "$BASE_DIR/missing_function_finder.php" "$BASE_DIR/../CLASSES" >> /tmp/phpcs_stats
