9b8895d85732b9664fa9c5cbe87a5b03c48301f7
Jon "The Nice Guy" Spriggs <jon@sprig.gs>
2012-06-17 23:31:16 +0100

    This class no longer throws an exception on instantiation.

--- Tests/classes/Base/ResponseTest.php
+++ Tests/classes/Base/ResponseTest.php
@@ -1,12 +1,9 @@
 <?php
 class Base_ResponseTest extends PHPUnit_Framework_TestCase
 {
-    /**
-     * @expectedException BadMethodCallException
-     */
     public function testInitialize()
     {
-        @$response = new Base_Response();
+        $response = new Base_Response();
     }
     
     
