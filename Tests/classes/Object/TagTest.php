83b5f8ee873971d824bfcfbee369d4d95900081f
Jon "The Nice Guy" Spriggs <jon@sprig.gs>
2012-08-08 11:15:09 +0100

    Tags haven't been used yet in the code - I can re-implement post-OggCamp.

--- Tests/classes/Object/TagTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Object_TagTest extends PHPUnit_Framework_TestCase
-{    
-    public function testObjectTagCreation()
-    {
-        $objTag = new Object_Tag();
-        $this->assertTrue(is_object($objTag));
-    }
-}
